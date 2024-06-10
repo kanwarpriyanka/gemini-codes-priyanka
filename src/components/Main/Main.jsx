fdc57e8c944b27c77239d6deed45197db659301e
Priyanka Kanwar <ebs.priyanka11@gmail.com>
2024-06-11 00:39:32 +0530

    gemini clone code

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -16,7 +16,7 @@ const Main = () => {
   return (
     <div className="main">
       <div className="nav">
-        <p>Gemini Codes priyanka</p>
+        <p>Gemini Codes-Priyanka</p>
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
